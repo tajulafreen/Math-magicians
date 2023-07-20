81d0b8851b4abb72b4843befcfdd1daaba279f57
aqurds <omarf1320@gmail.com>
2023-07-20 15:49:15 +0600

    Add test for calculate, operate

---
+++ src/logic/__test__/operate.test.js
@@ -0,0 +1,8 @@
+import operate from "../operate";
+
+test('operate function returns calculated result', () => {
+  expect(operate(2, 3, '+')).toBe('5');
+  expect(operate(3, 3, '-')).toBe('0');
+  expect(operate(5, 4, 'x')).toBe('20');
+  expect(operate(3, 3, '÷')).toBe('1');
+});
